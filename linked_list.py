67d83bb4f5261ac2028d6a2ede9d4e45a00d714f
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2026-03-27 18:09:34 +0200

    Step 3: implement append method

--- linked_list.py
+++ linked_list.py
@@ -23,3 +23,16 @@ class LinkedList:
             elements.append(str(current.data))
             current = current.next
         return " -> ".join(elements)
+
+    def append(self, value):
+        new_node = Node(value)
+
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+        self._size += 1
