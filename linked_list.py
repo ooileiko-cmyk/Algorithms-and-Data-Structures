2a6fdd5a2b077c6dfc9b931dedd23155a808a301
ooileiko-cmyk <o.o.ileiko@student.khai.edu>
2026-03-27 18:08:59 +0200

    Step 2: add LinkedList base structure

--- linked_list.py
+++ linked_list.py
@@ -2,3 +2,24 @@ class Node:
     def init(self, data):
         self.data = data
         self.next = None
+
+
+class LinkedList:
+    def init(self, iterable=None):
+        self.head = None
+        self._size = 0
+
+        if iterable:
+            for item in iterable:
+                self.append(item)
+
+    def len(self):
+        return self._size
+
+    def repr(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(str(current.data))
+            current = current.next
+        return " -> ".join(elements)
